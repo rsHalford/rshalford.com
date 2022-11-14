015b7c360fc4e925f840d4cab7891db1472ea3d5
Richard Halford <richard@xhalford.com>
2022-11-14 00:48:11 +0000

    refactor: move to a strict typescript codebase

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,9 +1,9 @@
 import rss from "@astrojs/rss";
 
-export const get = () =>({
+export const get = () => rss({
   title: "rshalford | Blog",
   description: "Latest articles published to rshalford.com, about all things software",
   site: "https://www.rshalford.com",
-  items: import.meta.glob("./**/*.md"),
+  items: import.meta.glob("./**/*.{md,mdx}"),
   cusomtData: `<language>en-gb</language>`,
 });
