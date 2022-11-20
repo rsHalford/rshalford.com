9a9fc84a3c17f7dddbc07261fe9628b7c9043fab
Richard Halford <richard@xhalford.com>
2022-11-20 13:49:30 +0000

    feat(mdx): support mdx filetypes for posts
    This also includes support for saving posts as drafts, adding a predicted
    reading time to posts, and the rose pine moon theme for syntax highlighting.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,18 @@
 import { defineConfig } from 'astro/config';
+import { remarkReadingTime } from './src/scripts/remark-reading-time.mjs';
 
-// https://astro.build/config
-export default defineConfig({});
+import mdx from '@astrojs/mdx';
+
+export default defineConfig({
+  markdown: {
+    drafts: true,
+    extendDefaultPlugins: true,
+    remarkPlugins: [remarkReadingTime],
+    shikiConfig: {
+      theme: "rose-pine-moon",
+    },
+  },
+  integrations: [mdx({
+    drafts: true,
+  })],
+});
