ff707b14efae74a4c81432c83abab23999a251c7
Richard Halford <richard@xhalford.com>
2022-11-20 17:28:01 +0000

    feat(sitemap): added sitemap generation for improved site crawling

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from "astro/config";
 import { remarkReadingTime } from "./src/scripts/remark-reading-time.mjs";
-
 import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
+// https://astro.build/config
 export default defineConfig({
+  site: "https://www.rshalford.com",
   markdown: {
     drafts: true,
     extendDefaultPlugins: true,
@@ -16,5 +18,6 @@ export default defineConfig({
     mdx({
       drafts: true,
     }),
+    sitemap(),
   ],
 });
